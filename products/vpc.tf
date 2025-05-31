7918688791b8aa9e9a36faabb360bccd6bfc098b
J Cole Morrison <me@jcolemorrison.com>
2025-05-30 19:58:15 -0700

    update to latest version of VPC module

--- products/vpc.tf
+++ products/vpc.tf
@@ -1,6 +1,6 @@
 module "vpc" {
-  source  = "app.terraform.io/hashicorp-team-demo/vpc-base/aws"
-  version = "0.4.0"
-  cidr_block             = var.vpc_cidr_block
-  name                   = "network-${var.project_name}"
+  source     = "app.terraform.io/hashicorp-team-demo/vpc-base/aws"
+  version    = "0.5.0"
+  cidr_block = var.vpc_cidr_block
+  name       = "network-${var.project_name}"
 }
