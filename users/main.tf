cd83eb10c661000c04aed064a8311c2f722c6d64
J Cole Morrison <me@jcolemorrison.com>
2025-05-30 21:21:56 -0700

    wip users

---
+++ users/main.tf
@@ -0,0 +1,35 @@
+resource "aws_security_group" "ec2" {
+  name        = "ec2-sg-${var.project_name}"
+  description = "Allow SSH"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Open to the world; restrict as needed
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+data "aws_ssm_parameter" "al2023" {
+  name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
+}
+
+resource "aws_instance" "example" {
+  ami                    = data.aws_ssm_parameter.al2023.value
+  instance_type          = "t3.micro"
+  associate_public_ip_address = true
+  subnet_id              = module.vpc.public_subnet_ids[0]
+  vpc_security_group_ids = [aws_security_group.ec2.id]
+
+  tags = {
+    Name = "example-ec2-${var.project_name}"
+  }
+}
--- users/providers.tf
+++ users/providers.tf
@@ -4,5 +4,16 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.99.1"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.7.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_default_region
+  default_tags {
+    tags = var.aws_default_tags
   }
 }
