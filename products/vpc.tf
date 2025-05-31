42a8128c88272034ac5af9caa13eca401ac7c606
J Cole Morrison <me@jcolemorrison.com>
2025-05-30 20:17:46 -0700

    use latest version of vpc module

--- products/vpc.tf
+++ products/vpc.tf
@@ -1,6 +1,6 @@
 module "vpc" {
   source     = "app.terraform.io/hashicorp-team-demo/vpc-base/aws"
-  version    = "0.5.0"
+  version    = "0.8.3"
   cidr_block = var.vpc_cidr_block
   name       = "network-${var.project_name}"
 }
