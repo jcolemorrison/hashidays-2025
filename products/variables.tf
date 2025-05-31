48cf5ca89a8e0abc210d83cc2d9f7d7e894a4a17
J Cole Morrison <me@jcolemorrison.com>
2025-05-30 19:45:17 -0700

    added default vars values

--- products/variables.tf
+++ products/variables.tf
@@ -1,9 +1,11 @@
 variable "project_name" {
   description = "Name of the project, used for resource naming."
   type        = string
+  default     = "hashidays_products"
 }
 
 variable "vpc_cidr_block" {
   description = "CIDR block for the VPC."
   type        = string
+  default     = "10.5.0.0/22"
 }
