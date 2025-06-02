bb9b8e74c88cedfb2ee6a39eee0e822dfb212ce0
J Cole Morrison <me@jcolemorrison.com>
2025-06-03 00:08:57 +0100

    revert vpc module version

--- payments/vpc.tf
+++ payments/vpc.tf
@@ -1,6 +1,6 @@
 module "vpc" {
   source     = "app.terraform.io/hashicorp-team-demo/vpc-base/aws"
-  version    = "0.8.2"
+  version    = "0.8.1"
   cidr_block = var.vpc_cidr_block
   name       = "network-${var.project_name}"
 }
